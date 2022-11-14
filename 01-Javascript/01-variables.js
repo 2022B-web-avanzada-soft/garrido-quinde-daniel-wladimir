25dadcee639ce775d77a93dea9ed5fe67b2c9431
Daniel Garrido <daniel.garrido@epn.edu.ec>
2022-11-14 17:55:52 -0500

    hasta variables primitivas

---
+++ 01-Javascript/01-variables.js
@@ -0,0 +1,29 @@
+// tipos de variables
+//mutables e inmatables
+
+
+// mutables que se pueden reasignar
+var numero1 = 1;
+let nueroDos = 2;
+
+numero1 = 13;
+nueroDos=dasd;
+
+
+
+//inmutables, no cambia es const
+const hola = "fas";
+
+//tipos de variables
+const numero = 1;
+const sueldo = 1.3;
+const texto = "adiran"
+const boolean = true;
+
+
+console.log(typeof numero)
+
+
+
+
+
